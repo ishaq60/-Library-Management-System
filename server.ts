eef3cdbe43df9e9699bd8d13e05c957c41303a83
Ishaq ahammad <ishaqahammad60@gmail.com>
2025-08-19 17:41:33 +0600

    setup Done

---
+++ server.ts
@@ -0,0 +1,20 @@
+import { Server } from "http";
+import app from "./src/app/app";
+const mongoose = require('mongoose');
+
+let server:Server
+const PORT=3000
+async function main() {
+  try{
+    await mongoose.connect('mongodb+srv://LibraryManagement:n1Akhhx2k5y5GmRT@cluster0.uqruf4z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+     console.log("connected to moongoed")
+    server=app.listen(PORT,()=>{
+        console.log(`app is listening on port ${PORT}`)
+    })
+
+  }
+catch(error) {
+console.log(error)
+}
+}
+main()
